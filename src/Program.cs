1e911867d4421c56577153f69439bea851052c56
Ishmaeel <Ishmaeel@users.noreply.github.com>
2023-06-09 20:50:01 +0300

    just pat

---
+++ src/Program.cs
@@ -0,0 +1,8 @@
+﻿var patt = Environment.GetEnvironmentVariable("PATH") ?? string.Empty;
+
+var pats = patt.Split(';');
+
+Array.Sort(pats, StringComparer.OrdinalIgnoreCase);
+
+foreach (var pat in pats)
+    Console.WriteLine(pat);
